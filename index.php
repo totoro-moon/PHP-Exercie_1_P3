2f4174cf4bf9cefad1b382c20d179ed3b71c801d
totoro-moon <flaviefrancois21@gmail.com>
2021-06-15 13:13:56 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1 Partie 3</title>
+</head>
+<body>
+    <h1># Exercice 1</h1>
+<p>Créez une fonction qui retourne un booléen.
+</p>
+<?php
+function booléen(){
+    return true;
+}
+echo booléen();
+?>
+</body>
+</html>
